d305ddb590b2d9146c6dfec2a632377412ee186c
Maxime Kuntz <maxime.kuntz75@gmail.com>
2024-04-10 22:34:04 +0200

    Split file

---
+++ R/print.palette.R
@@ -0,0 +1,23 @@
+#' Palette printer
+#'
+#' @param x A palette
+#' @param ... ...
+#'
+#' @importFrom graphics rect par image text
+#' @importFrom grDevices rgb
+#'
+#' @export
+print.palette <- function(x, ...) {
+  n <- length(x)
+  old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
+  on.exit(par(old))
+
+  image(1:n, 1,
+    as.matrix(1:n),
+    col = x,
+    ylab = "", xaxt = "n", yaxt = "n", bty = "n"
+  )
+
+  rect(0, 0.9, n + 1, 1.1, col = rgb(1, 1, 1, 0.8), border = NA)
+  text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 2, family = "sans")
+}
