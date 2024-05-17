10af6f56d2520652105ca95e4727a9e6959d0de2
Maxime Kuntz <maxime.kuntz75@gmail.com>
2024-05-17 10:42:02 +0200

    Improve GamesLook palette

--- R/colours.R
+++ R/colours.R
@@ -6,7 +6,7 @@
 paris2024_palettes <- list(
   Logo = c("#D7C378", "#000000"),
   OlympicRings = c("#0078D0", "#FFB114", "#000000", "#00A651", "#F0282D"),
-  GamesLook = c("#F1ABC9", "#D9C47A", "#2393CD", "#003561", "#61CFFF"),
+  GamesLook = c("#F1ABC9", "#D9C47A", "#61CFFF", "#003561", "#2393CD"),
   Impact2024 = c("#00004D", "#3EDE8E", "#006AFF"),
   Generation2024 = c("#00004D", "#FAB900", "#006AFF"),
   TerreDeJeux = c("#00004D", "#FF5757", "#006AFF"),
